8dc598bfe62b74c25ede185d68626023ddb638b0
LessIsMoreMK <maciej8kz@gmail.com>
2020-11-17 09:46:12 +0100

    Basic MVVM setup

---
+++ DayHelper/Helpers/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DayHelper
+{
+    /// <summary>
+    /// A base view model that fires Property Changed event as needed
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ DayHelper/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿namespace DayHelper
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Fields
+
+        private string _Tekst { get; set; } = "Asdf";
+
+        #endregion
+
+        #region Properties
+        public string Tekst
+        {
+            get => _Tekst;
+            set
+            {
+                if (_Tekst != value)
+                {
+                    _Tekst = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public RelayCommand StartCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public MainViewModel()
+        {
+            StartCommand = new RelayCommand(Start);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Start()
+        {
+            Tekst = "asdfasdf";
+        }
+
+        #endregion
+    }
+}
