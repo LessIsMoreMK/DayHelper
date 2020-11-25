5a56bf5f88ebad5644eabab8a2d4cba707adea12
Bish0p3 <lordboria.boryson@gmail.com>
2020-11-25 19:48:43 +0100

    MainWindow tweak

--- DayHelper/ViewModels/WindowViewModel.cs
+++ DayHelper/ViewModels/WindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace DayHelper
 {
@@ -20,10 +21,19 @@ namespace DayHelper
         /// </summary>
         private int mWindowRadius = 15;
 
+        private Brush backgroundBrush = Brushes.White;
+        private Color surfaceBrush;
+        private Color primaryBrush;
+        private Color secondaryBrush;
+        private Color onBackgroundBrush;
+        private Color onSurfaceBrush;
+        private Color onPrimaryBrush;
+        private Color onSecondaryBrush;
+
         #endregion
 
         #region Public Properties
-
+        public bool lightTheme = true;
         /// <summary>
         /// The size of the resize border around the window
         /// </summary>
@@ -53,6 +63,47 @@ namespace DayHelper
         /// </summary>
         public int TitleHeight { get; set; } = 42;
 
+        /// <summary>
+        /// Background brush property
+        /// </summary>
+        public Brush BackgroundBrush
+        {
+            get 
+            { 
+                return backgroundBrush; 
+            }
+            set 
+            {
+                backgroundBrush = value;
+                OnPropertyChanged("BackgroundBrush");
+            }
+        }
+
+        public Color SurfaceBrush
+        {
+            get
+            {
+                return surfaceBrush;
+            }
+            set
+            {
+                surfaceBrush = value;
+                OnPropertyChanged("SurfaceBrush");
+            }
+        }
+
+        public Color PrimaryBrush
+        {
+            get
+            {
+                return primaryBrush;
+            }
+            set
+            {
+                primaryBrush = value;
+                OnPropertyChanged("PrimaryBrush");
+            }
+        }
         #endregion
 
         #region Commands
@@ -77,6 +128,7 @@ namespace DayHelper
         /// </summary>
         public ICommand MenuCommand { get; set; }
 
+        public ICommand ThemeChangeCommand { get; set; }
         #endregion
 
         #region Constructor
@@ -102,10 +154,38 @@ namespace DayHelper
             CloseCommand = new RelayCommand(() => mWindow.Close());
             MenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
 
+            ThemeChangeCommand = new RelayCommand(() => ChangeTheme());
             // Fix window resize issue
             var resizer = new WindowResizer(mWindow);
+
+
         }
 
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Change theme Method
+        /// </summary>
+        /// <returns></returns>
+        public void ChangeTheme()
+        {
+            if (lightTheme == true)
+            {
+                BackgroundBrush = Brushes.Gray;
+                SurfaceBrush = (Color)ColorConverter.ConvertFromString("#403d39");
+                PrimaryBrush = (Color)ColorConverter.ConvertFromString("#eb5e28");
+
+                lightTheme = false;
+            }
+            else
+            {
+                BackgroundBrush = Brushes.White;
+                SurfaceBrush = (Color)ColorConverter.ConvertFromString("#ccc5b9");
+                PrimaryBrush = (Color)ColorConverter.ConvertFromString("#eb5e28");
+                lightTheme = true;
+            }
+        }
         #endregion
 
         #region Private Helpers
