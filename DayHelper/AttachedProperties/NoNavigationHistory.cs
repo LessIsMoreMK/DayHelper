d9d381e16ab0e580e1e81a4f880b584c6710cbbd
LessIsMoreMK <maciej8kz@gmail.com>
2020-12-14 23:51:14 +0100

    Removed navigation toolbar

---
+++ DayHelper/AttachedProperties/NoNavigationHistory.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DayHelper
+{
+    public class NoNavigationHistory : BaseAttachedProperty<NoNavigationHistory, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var frame = (sender as Frame);
+
+            //Hide navigation bar
+            frame.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+
+            //Clear history of navigation; prevent alt usable
+            frame.Navigated += (ss, ee) => ((Frame)ss).NavigationService.RemoveBackEntry();
+
+        }
+    }
+}
