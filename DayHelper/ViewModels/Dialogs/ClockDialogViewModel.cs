a699ab19ae67d7dd65e96dbe9ca9d03f156336ad
Bish0p3 <lordboria.boryson@gmail.com>
2021-01-21 15:37:53 +0100

    PomodoroClock

---
+++ DayHelper/ViewModels/Dialogs/ClockDialogViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DayHelper
+{
+    public class ClockDialogViewModel : WindowViewModel
+    {
+        #region Public Properties
+
+        public string Title { get; set; }
+
+        public Control Content { get; set; }
+
+        public Thickness OuterMarginThickness { get; set; } = new Thickness(1);
+        #endregion
+
+        #region Constructor
+
+        public ClockDialogViewModel(Window window) : base(window)
+        {
+            WindowMinimumWidth = 200;
+            WindowMinimumHeight = 100;
+            TitleHeight = 30;
+        }
+        #endregion
+
+        #region Methods
+
+        void startClock()
+        {
+
+        }
+        void pauseClock()
+        {
+
+        }
+        #endregion
+    }
+}
